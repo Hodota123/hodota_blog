3f8458de89df8e70c56a4bb9d5c0734d1a45f6b1
HODOTA <pvuong79@gmail.com>
2022-01-11 17:28:45 +0700

    require morgan again

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-//const morgan = require('morgan');
+const morgan = require('morgan');
 const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
 
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 
 // HTTP logger
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 // Template engine
 app.engine(
